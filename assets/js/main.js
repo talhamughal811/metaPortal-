f547e1555e45f5244c67adae2b5165a9293422c9
talhamughal811 <talha.shafique@viralsquare.org>
2022-08-30 13:43:49 +0500

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:9
+        }
+    }
+})
+
+$('.owl-carousel-2 ,.owl-carousel').owlCarousel({
+    rtl:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
